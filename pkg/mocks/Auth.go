804a9435f4fb0d337f73cbd3306a79f9495b2cc8
gidyon <gideon.ngk@gmail.com>
2020-11-29 15:09:41 +0300

    added mocks for the auth API

---
+++ pkg/mocks/Auth.go
@@ -0,0 +1,37 @@
+package mocks
+
+import (
+	"github.com/gidyon/micro/pkg/grpc/auth"
+	"github.com/gidyon/services/pkg/mocks/mocks"
+	"github.com/stretchr/testify/mock"
+)
+
+// AuthAPIMock is auth API
+type AuthAPIMock interface {
+	auth.API
+}
+
+// AuthAPI is a fake authentication API
+var AuthAPI = &mocks.AuthAPIMock{}
+
+func init() {
+	AuthAPI.On("AuthenticateRequestV2", mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthenticateRequest", mock.Anything).
+		Return(nil)
+	AuthAPI.On("AuthorizeActor", mock.Anything, mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthorizeActors", mock.Anything, mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthorizeGroups",
+		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthorizeStrict",
+		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthorizeActorOrGroups",
+		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("GenToken", mock.Anything, mock.Anything, mock.Anything).
+		Return("token", nil)
+}
