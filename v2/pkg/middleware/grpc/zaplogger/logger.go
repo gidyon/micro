e3a9ebb1764fbfe441764ebf20c704f235553d3a
gidyon <gideon.ngk@gmail.com>
2021-01-07 23:08:49 +0300

    added ZapGrpcLoggerV2 helper to get the underlying grpclog LoggerV2 from zap logger

--- v2/pkg/middleware/grpc/zaplogger/logger.go
+++ v2/pkg/middleware/grpc/zaplogger/logger.go
@@ -1,12 +1,14 @@
 package zaplogger
 
 import (
+	"fmt"
 	"os"
 	"sync"
 	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc/grpclog"
 )
 
 var (
@@ -77,4 +79,66 @@ func Init(lvl int, timeFormat string) error {
 	})
 
 	return err
+}
+
+// ZapGrpcLoggerV2 wraps a zap logger into a grpc LoggerV2
+func ZapGrpcLoggerV2(logger *zap.Logger) grpclog.LoggerV2 {
+	return &zapGrpcLoggerV2{logger: logger}
+}
+
+type zapGrpcLoggerV2 struct {
+	logger    *zap.Logger
+	verbosity int
+}
+
+func (l *zapGrpcLoggerV2) Info(args ...interface{}) {
+	l.logger.Info(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Infoln(args ...interface{}) {
+	l.logger.Info(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Infof(format string, args ...interface{}) {
+	l.logger.Info(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLoggerV2) Warning(args ...interface{}) {
+	l.logger.Warn(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Warningln(args ...interface{}) {
+	l.logger.Warn(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Warningf(format string, args ...interface{}) {
+	l.logger.Warn(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLoggerV2) Error(args ...interface{}) {
+	l.logger.Error(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Errorln(args ...interface{}) {
+	l.logger.Error(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Errorf(format string, args ...interface{}) {
+	l.logger.Error(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLoggerV2) Fatal(args ...interface{}) {
+	l.logger.Fatal(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Fatalln(args ...interface{}) {
+	l.logger.Fatal(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLoggerV2) Fatalf(format string, args ...interface{}) {
+	l.logger.Fatal(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLoggerV2) V(level int) bool {
+	return level <= l.verbosity
 }
