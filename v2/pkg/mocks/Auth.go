d6b55d52bcfec6dbfe7f62bd85580e4625a22244
gidyon <gideon.ngk@gmail.com>
2021-01-27 12:58:23 +0300

    calling authorize return mocked payload with all fields

--- v2/pkg/mocks/Auth.go
+++ v2/pkg/mocks/Auth.go
@@ -2,7 +2,9 @@ package mocks
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/Pallinder/go-randomdata"
 	"github.com/gidyon/micro/v2/pkg/middleware/grpc/auth"
 	"github.com/gidyon/micro/v2/pkg/mocks/mocks"
 	"github.com/stretchr/testify/mock"
@@ -18,19 +20,19 @@ var AuthAPI = &mocks.AuthAPIMock{}
 
 func init() {
 	AuthAPI.On("AuthenticateRequest", mock.Anything).Return(nil)
-	AuthAPI.On("AuthenticateRequestV2", mock.Anything).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
+	AuthAPI.On("AuthenticateRequestV2", mock.Anything).Return(mockPayload(), nil)
 	AuthAPI.On("AuthorizeFunc", anything()...).Return(context.Background(), nil)
-	AuthAPI.On("AuthorizeGroup", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeActor", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeActors", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeActorAndGroup", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeActorOrGroup", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeAdmin", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
-	AuthAPI.On("AuthorizeAdminStrict", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
+	AuthAPI.On("AuthorizeGroup", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeActor", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeActors", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeActorAndGroup", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeActorOrGroup", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeAdmin", anything()...).Return(mockPayload(), nil)
+	AuthAPI.On("AuthorizeAdminStrict", anything()...).Return(mockPayload(), nil)
 	AuthAPI.On("AdminGroups").Return([]string{auth.DefaultAdminGroup(), auth.DefaultAdminGroup()})
 	AuthAPI.On("IsAdmin", mock.Anything).Return(true)
 	AuthAPI.On("GenToken", anything()...).Return("token", nil)
-	AuthAPI.On("GetJwtPayload", anything()...).Return(&auth.Payload{Group: auth.DefaultAdminGroup()}, nil)
+	AuthAPI.On("GetJwtPayload", anything()...).Return(mockPayload(), nil)
 }
 
 func anything() []interface{} {
@@ -39,4 +41,14 @@ func anything() []interface{} {
 		v = append(v, mock.Anything)
 	}
 	return v
+}
+
+func mockPayload() *auth.Payload {
+	return &auth.Payload{
+		Group:        auth.DefaultAdminGroup(),
+		Names:        randomdata.SillyName(),
+		ProjectID:    "test",
+		EmailAddress: randomdata.Email(),
+		ID:           fmt.Sprint(randomdata.Number(1, 10)),
+	}
 }
